2b45fea798079f6c2ec8d78f6410f0310fa5f13b
Liem Nguyen <liemnguyen0111@gmail.com>
2020-05-13 01:49:23 -0700

    Added code to function

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,35 @@
-const questions = [
+const {prompt} = require('inquirer')
+const {getUser} = require('../develop/utils/api.js')
+const list = ['Project title', 'Description','Table of contents','Installation','Usage','License', 'Contributing', 'Tests','Questions']
+const questions = []
 
-];
+list.forEach(item =>
+{
+    questions.push({
+        type: 'input',
+        name: item,
+        message: item
+    })
+})
 
 function writeToFile(fileName, data) {
+
+   data.then(({data}) => {})
+
+   prompt(questions)
+   .then(resp => console.log(resp))
 }
 
 function init() {
-
+    prompt({
+        type: 'input',
+        name: 'username',
+        message: 'What is your username? '
+    })
+    .then(({username}) => {
+      writeToFile('Readme.md',getUser(username)) 
+    })
+    .catch(err => console.log(err))
 }
 
 init();
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,7 +1,16 @@
-const api = {
-  getUser(username) {
 
-  }
+const axios = require('axios')
+
+const api = {
+getUser(username) {
+  return axios.get((`https://api.github.com/users/${username}`))
+}
 };
 
+
 module.exports = api;
+// axios.get(`https://api.github.com/users/${username}`)
+
+// axios.get(`https://api.github.com/users/${username}`)
+// .then(data => {return data})
+// .catch(err => console.log(err))
